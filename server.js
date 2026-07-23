6158e611443f56ca6427a297f26ad4a92c9d88e3
waleedrehmandeveloper <waleedrehman.developer@gmail.com>
2026-07-23 11:50:17 +0500

    Use dynamic port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000 successfully")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} successfully`);
+});
